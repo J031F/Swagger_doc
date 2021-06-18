ae78e72326587222024b8b8e2a035c8fe83e8f70
J031Fm <joey.felip11@gmail.com>
2021-06-17 19:21:42 -0500

    first commit

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,25 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const userExist = this.usersRepository.findByEmail(email);
+
+    if (userExist) {
+      throw new Error("Email já em uso!");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,22 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const userExist = this.usersRepository.findById(user_id);
+
+    if (!userExist || !userExist.admin) {
+      throw new Error("Usuário não existe ou não é administrador!");
+    }
+
+    return this.usersRepository.list();
+  }
+}
+
+export { ListAllUsersUseCase };
